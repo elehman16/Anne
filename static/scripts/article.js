b9597be8857e76044ecb07b1444afa696d805aab
Derek Schuster <derrreks@gmail.com>
2018-01-04 18:08:30 -0500

    Add checkbox option

--- static/scripts/article.js
+++ static/scripts/article.js
@@ -1,3 +1,14 @@
+function preventMultiCheck() {
+    var box = $(this);
+    if (box.is(":checked")) {
+        var group = "input:checkbox";
+        $(group).prop("checked", false);
+        box.prop("checked", true);
+    } else {
+        box.prop("checked", false);
+    }
+}
+
 function getSelectionText() {
     var text = "";
 
@@ -15,9 +26,9 @@ function getSelectionText() {
 function isAlreadyHighlighted(highlighted) {
   var highlights = getFinalText();
   for (var i = 0; i < highlights.length; i++) {
-    if (highlights[i].includes(highlighted)) {
-      return true;
-    }
+      if (highlights[i].includes(highlighted)) {
+          return true;
+      }
   }
   return false;
 }
@@ -26,14 +37,14 @@ function add() {
     var highlighted = getSelectionText();
 
     if (isAlreadyHighlighted(highlighted) === true) {
-      $("#warning").append("<p>Text has already been selected.</p>")
-      return;
+        $("#warning").append("<p>Text has already been selected.</p>")
+        return;
     }
 
     $("#selected").append("<li>" + highlighted + "</li>");
     $("#text").html($("#text").html().replace(
-      highlighted,
-      "<span style=\"font-weight: bold;\">" + highlighted + "</span>")
+        highlighted,
+        "<span style=\"font-weight: bold;\">" + highlighted + "</span>")
     );
     $("#warning").empty();
 }
@@ -42,17 +53,27 @@ function getFinalText() {
     var results = [];
     var annotations = $("#selected li");
     annotations.each(function(idx, li) {
-      results.push($(li).text());
+        results.push($(li).text());
     });
     return results;
+}
+
+function getSelection() {
+    var choice = ""
+    $("input:checkbox:checked").each(function(){
+        choice = $(this).next("label").text();
+    });
 }
 
 function submit() {
     var userid = document.getElementById("userid").innerHTML;
     var id = document.getElementById("id").innerHTML;
     var annotations = getFinalText();
+    var selection = getSelection();
     if (annotations.length > 0) {
-        post("/submit/", {"userid": userid, "id": id, "annotations": JSON.stringify(annotations)});
+        post("/submit/", {"userid": userid, "id": id,
+                          "annotations": JSON.stringify(annotations),
+                          "selection": selection});
     }
 }
 
@@ -61,11 +82,12 @@ function clear() {
     $("#warning").empty();
     $("#warning").hide();
     $("#abstract").html($("#abstract").html().replace(
-      new RegExp("<span.+\">|<\/span>", "g"),
-      "")
+        new RegExp("<span.+\">|<\/span>", "g"),
+        "")
     );
 }
 
+$("input:checkbox").click(preventMultiCheck);
 $("#add-but").click(add);
 $("#submit-but").click(submit);
 $("#clear-but").click(clear);
